4fb8dd129aa87dfb22f31499f82b7fdb1677b1d9
Taster101 <Davontahenderson1@gmail.com>
2023-03-23 12:01:40 -0500

    added javascript functions

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,27 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var topic = topics[Math.floor(Math.random() * topics.length)];
 
 
 
-
-
+function listTopic(){
 for(var x = 0; x < topics.length; x++){
 console.log(topics[x])
 }
-
-/*if (topics === 'HTML') {
+}
+function selectTopic(){
+if (topic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
+} else if (topic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topics === 'Git') {
+} else if (topic === 'Git') {
   console.log("Let's study Git!");
-} else if (topics === 'JavaScript') {
+} else if (topic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
-} */
+} 
+}
+console.log('Here are the topics we learned through Prework:');
+listTopic();
+console.log('Which topic should we study first?');
+selectTopic();
